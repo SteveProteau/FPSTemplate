84b7fd733bdd0604cf121939c0901ea05d491e1d
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-02-24 18:40:48 -0800

    Create dedicated servers module

---
+++ Source/DedicatedServers/DedicatedServers.Build.cs
@@ -0,0 +1,15 @@
+
+using UnrealBuildTool;
+
+public class DedicatedServers : ModuleRules
+{
+	public DedicatedServers(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine"});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+	}
+}
--- Source/FPSTemplate/FPSTemplate.Build.cs
+++ Source/FPSTemplate/FPSTemplate.Build.cs
@@ -8,7 +8,7 @@ public class FPSTemplate : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PhysicsCore", "GameliftServerSDK" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PhysicsCore", "GameliftServerSDK", "DedicatedServers" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "Slate", "SlateCore" });
 
