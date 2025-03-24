2f6deedf5033ff3802cb1ebebb388288930d02d1
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-03-24 14:42:45 -0700

    Validate email and password

--- Source/DedicatedServers/Private/UI/Portal/SignIn/SignUpPage.cpp
+++ Source/DedicatedServers/Private/UI/Portal/SignIn/SignUpPage.cpp
@@ -2,3 +2,119 @@
 
 
 #include "UI/Portal/SignIn/SignUpPage.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/TextBlock.h"
+
+void USignUpPage::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	TextBox_UserName->OnTextChanged.AddDynamic(this, &USignUpPage::UpdateSignUpButtonState);
+	TextBox_Password->OnTextChanged.AddDynamic(this, &USignUpPage::UpdateSignUpButtonState);
+	TextBox_ConfirmPassword->OnTextChanged.AddDynamic(this, &USignUpPage::UpdateSignUpButtonState);
+	TextBox_Email->OnTextChanged.AddDynamic(this, &USignUpPage::UpdateSignUpButtonState);
+	Button_SignUp->SetIsEnabled(false);
+}
+
+void USignUpPage::UpdateSignUpButtonState(const FText& Text)
+{
+	const bool bIsUsernameValid = !TextBox_UserName->GetText().ToString().IsEmpty();
+	const bool bArePasswordsEqual = TextBox_Password->GetText().ToString() == TextBox_ConfirmPassword->GetText().ToString();
+	const bool bIsValidEmail = IsValidEmail(TextBox_Email->GetText().ToString());
+	const bool bIsPasswordLongEnough = TextBox_Password->GetText().ToString().Len() >= 8;
+ 
+	FString StatusMessage;
+	const bool bIsStrongPassword = IsStrongPassword(TextBox_Password->GetText().ToString(), StatusMessage);
+	if (!bIsStrongPassword)
+	{
+		TextBlock_StatusMessage->SetText(FText::FromString(StatusMessage));
+	}
+	else if (!bIsUsernameValid)
+	{
+		TextBlock_StatusMessage->SetText(FText::FromString(TEXT("Please enter a valid Username.")));
+	}
+	else if (!bArePasswordsEqual)
+	{
+		TextBlock_StatusMessage->SetText(FText::FromString(TEXT("Please ensure that passwords match.")));
+	}
+	else if (!bIsValidEmail)
+	{
+		TextBlock_StatusMessage->SetText(FText::FromString(TEXT("Please enter a valid email.")));
+	}
+	else if (!bIsPasswordLongEnough)
+	{
+		TextBlock_StatusMessage->SetText(FText::FromString(TEXT("Passwords must be at least 8 characters.")));
+	}
+	else
+	{
+		TextBlock_StatusMessage->SetText(FText::GetEmpty());
+	}
+	Button_SignUp->SetIsEnabled(bIsUsernameValid && bArePasswordsEqual && bIsValidEmail && bIsStrongPassword);
+}
+
+bool USignUpPage::IsValidEmail(const FString& Email)
+{
+	// Regular expression pattern:
+	//
+	//  R"(())" - Raw string to avoid Needing escape sequences (can include raw '\')
+	//  ^ starts string
+	//  [] character class matches any character from a set of characters - "one or more characters"
+	//  []+ for one or more characters
+	//  s white space
+	//  \s no white space
+	//  ^\s@ no white spaces and no @ symbol (here ^ is for start of pattern)
+	//  ^[^\s@]+  First sequence of characters should have no white spaces or @ symbol
+	//  @[^\s@]+ Second sequence of characters must start with @ then have no spaces or @ symbols
+	//  \. required '.'  (why backslash?)
+	//  {2,} at least 2 characters
+	//  $ end of string
+	//  [^\s@]{2,}$  no white spaces or @, at least 2 characters
+
+	//
+	const FRegexPattern EmailPattern(TEXT(R"((^[^\s@]+@[^\s@]+\.[^\s@]{2,}$))"));
+	FRegexMatcher Matcher(EmailPattern, Email);
+	return Matcher.FindNext();
+}
+ 
+bool USignUpPage::IsStrongPassword(const FString& Password, FString& StatusMessage)
+{
+	// Regular expression pattern:
+	//
+	//  w word character
+	//	\w special character or space (not word character)
+	//  \w\s special character
+	
+	const FRegexPattern NumberPattern(TEXT(R"(\d)")); // contains at least one number
+	const FRegexPattern SpecialCharPattern(TEXT(R"([^\w\s])")); // contains at least one special character
+	const FRegexPattern UppercasePattern(TEXT(R"([A-Z])")); // contains at least one uppercase character
+	const FRegexPattern LowercasePattern(TEXT(R"([a-z])")); // contains at least one lowercase character
+ 
+	FRegexMatcher NumberMatcher(NumberPattern, Password);
+	FRegexMatcher SpecialCharMatcher(SpecialCharPattern, Password);
+	FRegexMatcher UppercaseMatcher(UppercasePattern, Password);
+	FRegexMatcher LowercaseMatcher(LowercasePattern, Password);
+ 
+	if (!NumberMatcher.FindNext())
+	{
+		StatusMessage = TEXT("Password must contain at least 1 number.");
+		return false;
+	}
+	if (!SpecialCharMatcher.FindNext())
+	{
+		StatusMessage = TEXT("Password must contain at least one special character.");
+		return false;
+	}
+	if (!UppercaseMatcher.FindNext())
+	{
+		StatusMessage = TEXT("Password must contain at least one uppercase character.");
+		return false;
+	}
+	if (!LowercaseMatcher.FindNext())
+	{
+		StatusMessage = TEXT("Password must contain at least one lowercase character.");
+		return false;
+	}
+	return true;
+}
